70ead6810ac7246e5cb1705be2b56e75b969ebbf
nomisum <nomisum@googlemail.com>
2018-10-31 18:14:11 +0100

    added rotary sounds to cfgsounds

--- grad-landline/cfgSounds.hpp
+++ grad-landline/cfgSounds.hpp
@@ -1,3 +1,6 @@
+
+
+
 class GRAD_landline_phoneBeep1
 {
     name = "phoneBeep1";
@@ -57,5 +60,148 @@ class GRAD_landline_phoneHangUp
 {
     name = "phoneHangUp";
     sound[] = {"\grad-landline\sounds\phoneHangUp.ogg",2,1};
+    titles[] = {};
+};
+
+
+
+
+class GRAD_landline_rotary_0_down
+{
+    name = "rotary_0_down";
+    sound[] = {"\grad-landline\sounds\rotary_0_down.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_0_up
+{
+    name = "rotary_0_up";
+    sound[] = {"\grad-landline\sounds\rotary_0_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_1_down
+{
+    name = "rotary_1_down";
+    sound[] = {"\grad-landline\sounds\rotary_1_down.ogg",1.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_1_up
+{
+    name = "rotary_1_up";
+    sound[] = {"\grad-landline\sounds\rotary_1_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_2_down
+{
+    name = "rotary_2_down";
+    sound[] = {"\grad-landline\sounds\rotary_2_down.ogg",2.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_2_up
+{
+    name = "rotary_2_up";
+    sound[] = {"\grad-landline\sounds\rotary_2_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_3_down
+{
+    name = "rotary_3_down";
+    sound[] = {"\grad-landline\sounds\rotary_3_down.ogg",3.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_3_up
+{
+    name = "rotary_3_up";
+    sound[] = {"\grad-landline\sounds\rotary_3_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_4_down
+{
+    name = "rotary_4_down";
+    sound[] = {"\grad-landline\sounds\rotary_4_down.ogg",4.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_4_up
+{
+    name = "rotary_4_up";
+    sound[] = {"\grad-landline\sounds\rotary_4_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_5_down
+{
+    name = "rotary_5_down";
+    sound[] = {"\grad-landline\sounds\rotary_5_down.ogg",5.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_5_up
+{
+    name = "rotary_5_up";
+    sound[] = {"\grad-landline\sounds\rotary_5_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_6_down
+{
+    name = "rotary_6_down";
+    sound[] = {"\grad-landline\sounds\rotary_6_down.ogg",6.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_6_up
+{
+    name = "rotary_6_up";
+    sound[] = {"\grad-landline\sounds\rotary_6_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_7_down
+{
+    name = "rotary_7_down";
+    sound[] = {"\grad-landline\sounds\rotary_7_down.ogg",7.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_7_up
+{
+    name = "rotary_7_up";
+    sound[] = {"\grad-landline\sounds\rotary_7_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_8_down
+{
+    name = "rotary_8_down";
+    sound[] = {"\grad-landline\sounds\rotary_8_down.ogg",8.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_8_up
+{
+    name = "rotary_8_up";
+    sound[] = {"\grad-landline\sounds\rotary_8_up.ogg",0.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_9_down
+{
+    name = "rotary_9_down";
+    sound[] = {"\grad-landline\sounds\rotary_9_down.ogg",9.75,1};
+    titles[] = {};
+};
+
+class GRAD_landline_rotary_9_up
+{
+    name = "rotary_9_up";
+    sound[] = {"\grad-landline\sounds\rotary_9_up.ogg",0.75,1};
     titles[] = {};
 };
